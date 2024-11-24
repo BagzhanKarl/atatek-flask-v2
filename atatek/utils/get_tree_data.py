9e664c5d82f0c324a99f6f191a74eb139bb32bb4
novia <bagzhankarl@gmail.com>
2024-11-24 06:22:47 +0500

    Second VERSION - ready for half

--- atatek/utils/get_tree_data.py
+++ atatek/utils/get_tree_data.py
@@ -2,5 +2,13 @@ import requests
 
 def get_tree_data_on_request(id):
     url = f'https://tumalas.kz/wp-admin/admin-ajax.php?action=tuma_cached_childnew_get&nodeid=14&id={id}'
-    response = requests.get(url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1'
+    }
+
+    response = requests.get(url, headers=headers)
     return response.json()
