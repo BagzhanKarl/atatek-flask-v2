4973ffbd0ccaebe405ef04b3c89d44957dad227b
novia <bagzhankarl@gmail.com>
2024-11-24 06:08:00 +0500

    Second VERSION - ready for half

--- atatek/utils/get_tree_data.py
+++ atatek/utils/get_tree_data.py
@@ -3,4 +3,6 @@ import requests
 def get_tree_data_on_request(id):
     url = f'https://tumalas.kz/wp-admin/admin-ajax.php?action=tuma_cached_childnew_get&nodeid=14&id={id}'
     response = requests.get(url)
+    print(response.text)  # Это покажет текстовый ответ
+    print(response.status_code)
     return response.json()
